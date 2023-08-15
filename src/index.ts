171394dd22f14ffcb717bbeba21968002c0c73b6
nekosu <liao.hy@outlook.com>
2023-08-16 01:17:20 +0800

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './framework'
 export * from './framework/wrapper'
 export * from './core'
+export * from './core/wrapper'
--- test/core.ts
+++ test/core.ts
@@ -1,8 +1,12 @@
-import { MaaCoreLoader } from '..'
+import { AsstInstance, MaaCoreLoader } from '..'
 
 async function main() {
   const loader = new MaaCoreLoader()
   loader.load('./install.core')
+
+  const instance = new AsstInstance(loader, (msg, details) => {
+    console.log(msg, JSON.parse(details))
+  })
 }
 
 main()
